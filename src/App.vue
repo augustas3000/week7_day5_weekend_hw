835ed95020249affa48ff5215f066d0753a52cc7
augustas3000 <augusto.pasaulis@gmail.com>
2020-02-23 13:34:54 +0000

    Search bar added, linked to map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+
+  <div class="app-main-container">
+
+    <header>
+      <h1>HEADER</h1>
+    </header>
+
+    <div class="map-and-sidebar-container">
+      <div class="side-bar">
+        <div class="form-goup">
+            <div class="">
+                <input v-model="munroSearchString" type="text" name="" value="" placeholder="Search munros">
+            </div>
+        </div>
+
+      </div>
+
+        <!-- make munros render on the map based on search bar results -->
+        <div class="map-container">
+
+          <my-map v-if="munroSearchString" v-bind:munros="munrosFiltered"></my-map>
+          <my-map v-else v-bind:munros="munros"></my-map>
+        </div>
+
+    </div>
+
+
   </div>
+
+
+
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { eventBus } from "@/main.js";
+// import VuedogBeerList from "@/components/VuedogBeerList";
+// import VuedogBeerInfo from "@/components/VuedogBeerInfo";
+// import VuedogHeader from "@/components/VuedogHeader";
+// import VuedogFavouritesList from "@/components/VuedogFavouritesList";
+import MyMap from './components/MyMap.vue'
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    "my-map": MyMap
+  },
+  data() {
+    return {
+      munros: [],
+      munroSearchString: ""
+    };
+  },
+  computed: {
+    munrosFiltered() {
+
+      if (this.munroSearchString) {
+        return this.munros.filter(munro => {
+          return munro.name.toLowerCase().includes(this.munroSearchString.toLowerCase())
+        })
+      }
+    }
+  },
+  methods: {
+      getMunros: function() {
+      fetch('https://munroapi.herokuapp.com/munros').then(res => res.json()).then(munros => this.munros = munros)
+      }
+  },
+
+  mounted() {
+    this.getMunros();
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
+
+
+  .map-and-sidebar-container {
+    display: flex;
+    flex-grow: 1;
+    flex-flow: row;
+  }
+
+  .side-bar {
+    width: 20%;
+  }
+
+  .map-container {
+    width: 80%;
+  }
+
 </style>
---
+++ src/components/MyMap.vue
@@ -0,0 +1,405 @@
+<template>
+
+  <div style="height: 80vh; width: 100%">
+    <div style="height: 300px overflow: auto;">
+      <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
+      <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
+      <button @click="showLongText">
+        Toggle long popup
+      </button>
+      <button @click="showMap = !showMap">
+        Toggle map
+      </button>
+    </div>
+
+
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 100%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+
+      <l-marker v-for="(munro, index) in munros" :lat-lng="[munro.latlng_lat, munro.latlng_lng]" v-bind:key="index">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+
+
+    </l-map>
+  </div>
+
+
+
+</template>
+
+<script>
+
+import L from 'leaflet';
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
+
+export default {
+  name: "my-map",
+  props: ['munros'],
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 8,
+      center: latLng(57.067617, -5.097655),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(47.41322, -1.219482),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 10,
+      currentCenter: latLng(57.097468, -3.765564),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  }
+};
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
+
+  <div class="map-container-leaflet">
+    <div class="map-container-info">
+
+      <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
+
+      <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
+
+
+
+      <button @click="showLongText">
+        Toggle long popup
+      </button>
+
+
+      <button @click="showMap = !showMap">
+        Toggle map
+      </button>
+
+
+    </div>
+
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+
+ -->
+<!-- latLng(47.41322, -1.219482) -->
+<!-- <l-marker v-for="(munro, index) in munros" v-bind:lat-lng="withPopup">
+  <l-popup>
+    <div @click="innerClick">
+      I am a popup
+      <p v-show="showParagraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+        Donec finibus semper metus id malesuada.
+      </p>
+    </div>
+  </l-popup>
+</l-marker>
+
+ -->
+
+      <!-- <l-marker v-for="(munro, index) in munros" :lat-lng="[munro.latlng_lat, munro.latlng_lng]" v-bind:key="index">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker> -->
+
+
+
+
+
+<!--
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div @click="innerClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-tooltip>
+      </l-marker> -->
+
+<!--
+
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
+export default {
+  name: "my-map",
+  props: ['munros'],
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 7,
+      center: latLng(56.820186, -5.105362),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(47.41322, -1.219482),
+      // withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 7,
+      currentCenter: latLng(56.820186, -5.105362),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  },
+  computed: {
+
+  }
+};
+</script>
+
+
+<style lang="css" scoped>
+
+  .map-container-leaflet {
+    height: 500px;
+    width: 100%;
+  }
+
+  .map {
+    width: 100%;
+  }
+
+  .map-container-info {
+    height: 120px;
+    overflow: auto;
+  }
+
+
+
+</style> -->
+<!--
+<template>
+
+  <div style="height: 500px; width: 100%">
+    <div style="height: 200px overflow: auto;">
+      <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
+      <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
+      <button @click="showLongText">
+        Toggle long popup
+      </button>
+      <button @click="showMap = !showMap">
+        Toggle map
+      </button>
+    </div>
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 80%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-marker :lat-lng="withPopup">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div @click="innerClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+
+export default {
+  name: "Example",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(47.41322, -1.219482),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(47.41322, -1.219482),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(47.41322, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  }
+};
+</script> -->
